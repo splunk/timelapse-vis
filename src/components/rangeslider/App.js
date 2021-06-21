2b10e69c05971ad619efed7081368fe6b2f44323
Ryan O'Connor <ryan.oconnor@gmail.com>
2021-06-21 17:58:50 -0400

    Updating rangeslider code with comments

--- src/components/rangeslider/App.js
+++ src/components/rangeslider/App.js
@@ -5,86 +5,109 @@ import packageJson from '../../../package.json';
 import { ThemeProvider } from 'styled-components';
 import EnterprisePreset, { themes as presetThemes } from '@splunk/dashboard-presets/EnterprisePreset';
 import DashboardCore, { themes as dashboardCoreThemes } from '@splunk/dashboard-core';
+import DashboardCoreApi from '@splunk/dashboard-core';
+import { DashboardContextProvider } from '@splunk/dashboard-context';
+import GeoRegistry from '@splunk/dashboard-context/GeoRegistry';
+import GeoJsonProvider from '@splunk/dashboard-context/GeoJsonProvider';
 import { themes as reactUIThemes } from '@splunk/react-ui/themes';
 
 
+//Get the current dashboard ID
 const dashboard_id = window.location.pathname.split("/").pop()
-const themeKey = 'enterpriseDark';
 
+//Set the themeKey to enterpriseDark
+const themeKey = 'enterpriseDark';
 const theme = {
     ...presetThemes[themeKey],
     ...dashboardCoreThemes[themeKey],
     ...reactUIThemes[themeKey],
 };
 
+//Initialize Variables as empty
 var selectedInterval = []
 var disabledIntervals = []
 var timelineInterval = []
 var def = {}
 
+
+const geoRegistry = GeoRegistry.create();
+geoRegistry.addDefaultProvider(new GeoJsonProvider());
+
+
+//Look through packageJson to find if there is a matching dashboard_id that needs a rangeslider
 for (let timelapse_index = 0; timelapse_index < packageJson.timesliders.length; timelapse_index++)
 {
     if (packageJson.timesliders[timelapse_index].dashboard_id === dashboard_id){
-    	selectedInterval = packageJson.timesliders[timelapse_index].selectedInterval
-
-        selectedInterval.forEach(function(part, index, theArray) {
-            theArray[index] = new Date(theArray[index]);
-        });
-        
-	disabledIntervals = []
+	//Setup the selectedInterval and timelineInterval as the last 30 days
         var today = new Date();
-
         var thirtyDaysAgo = today - 1000 * 60 * 60 * 24 * 30;
-        console.log(today)
-	console.log(thirtyDaysAgo)
         timelineInterval=[thirtyDaysAgo, today]
         selectedInterval = timelineInterval	
   }
 }
 
+
+//SplunkTimeRangeSlider Class
 class SplunkTimeRangeSlider extends React.Component {
+  
+  //Initialize State
   state = {
     error: false,
     selectedInterval,
     def: this.props.dash.props.definition
   };
   
+  //Create DashboardCore using the passed definition
   dash = <DashboardCore
             width="100%"
             height="calc(100vh - 78px)"
             definition={this.state.def}
+            onDefinitionChange={console.log("Changed!")}
             preset={EnterprisePreset} />
 
+  //Create start_range as 0
   start_range = 0 
+
+  //Create End Range as Today's Date
   end_range =  Math.round(Date.now() / 1000)
+  
   errorHandler = ({ error }) => this.setState({ error });
+  
+  //Function for handling range slider changes
   onChangeCallback = (selectedInterval) => {
 
- selectedInterval.map((d, i) => {
+
+  //Update the selectedInterval variable with the new start and end times
+  selectedInterval.map((d, i) => {
           if (i == 0){
           this.start_range = d.getTime() / 1000;
           }
           if (i==1){
-            
 	  this.end_range = d.getTime() / 1000;
-}
-        })
+         }
+   })
    
-console.log(this.start_range)
-console.log(this.end_range)
 
-this.setState({ selectedInterval })
-for(var v in this.state.def.dataSources)
-{
-this.state.def.dataSources[v].options.query = "| makeresults count=100 | eval t=86400 | streamstats sum(t) as t | eval _time=_time-t | eval time=_time | where time<=" + this.end_range.toString() + " AND time>=" + this.start_range.toString()+" | fields - time"
-}
+  //Set the state variable of selectedInterval with the new values
+  this.setState({ selectedInterval })
 
-this.setState({
-        def: this.state.def
-}
-)
-//this.dash.updateDefinition(this.state.def)
-};
+  //For each dataSource in the dashboard, append a where clause to limit the start/end time
+  var definition_new = this.state.def
+  for(var v in definition_new.dataSources)
+  {
+
+  //Need to update this later, currently just replacing the entire search with a new range based on the rangeslider selected start and end
+  definition_new.dataSources[v].options.query = "| makeresults count=100 | eval t=86400 | streamstats sum(t) as t | eval _time=_time-t | eval time=_time | where time<=" + this.end_range.toString() + " AND time>=" + this.start_range.toString()+" | fields - time"
+  }
+
+  //Set the state with the new definition
+  this.setState({def: definition_new}, function() {
+  console.log("Updated")
+  });
+
+  //this.DashboardCoreApi.updateDefinition(this.state.def)
+  console.log(this.dash);
+  };
 
   render() {
     const { selectedInterval, def, error } = this.state;
@@ -112,14 +135,15 @@ this.setState({
           disabledIntervals={disabledIntervals}
         />
 
-<div>
-<ThemeProvider theme={theme}>
+        <div>
+          <ThemeProvider theme={theme}>
+          <DashboardContextProvider geoRegistry={geoRegistry}>
+          {this.dash}
+          </DashboardContextProvider>
 
-{this.dash}
-
-</ThemeProvider>
-</div>
-</div>
+          </ThemeProvider>
+        </div>
+      </div>
     );
   }
 }
