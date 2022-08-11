49e8124d61435351c0000de87236d96672850b88
Ryan O'Connor <ryan.oconnor@gmail.com>
2022-08-11 09:11:00 -0400

    Adjusting getDashboardDefinition API to use splunk/splunk-utils. Will need to validate this is working as we get closer.

--- packages/timelapse-visual/src/apis/getDashboardDefinition.ts
+++ packages/timelapse-visual/src/apis/getDashboardDefinition.ts
@@ -1,16 +1,16 @@
 import { DashboardDefinition } from "../data/dashboardDefinition";
+import { createURL } from "@splunk/splunk-utils/url";
+import { defaultFetchInit, handleResponse } from "@splunk/splunk-utils/fetch";
 
 export async function getDashboardDefinition(
   dashboardId: string
 ): Promise<DashboardDefinition> {
   try {
-    return await fetch(
-      `/splunkd/servicesNS/-/-/data/ui/views/${dashboardId}?output_mode=json`,
-      {
-        credentials: "include",
-      }
-    )
-      .then((res) => res.json())
+    const url: string = createURL(
+      `/splunkd/servicesNS/-/-/data/ui/views/${dashboardId}?output_mode=json`
+    );
+    return await fetch(url, defaultFetchInit)
+      .then((res) => handleResponse(200))
       .then((data) => {
         const xml = new DOMParser().parseFromString(
           data.entry[0].content["eai:data"],
